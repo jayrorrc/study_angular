9c5a67e8cc6e67d52cba42360ac6a4f5fe21425e
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-05-31 19:17:38 -0300

    Fix search

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -33,11 +33,12 @@ export class PhotoListComponent implements OnInit {
     this.photoService
         .listFromUserPaginated(this.userName, ++this.currentPage)
         .subscribe(photos => {
-            this.photos = this.photos.concat(photos);
+          this.filter = '';
+          this.photos = this.photos.concat(photos);
 
-            if (!photos.length) {
-              this.hasMore = false;
-            }
+          if (!photos.length) {
+            this.hasMore = false;
+          }
         });
   }
 }
