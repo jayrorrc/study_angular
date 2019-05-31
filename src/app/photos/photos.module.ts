4efc192fc260572170417d008aeb7c3f3eb54c9b
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-05-31 00:04:27 -0300

    Add PhotoList

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
-import { PhotoComponent } from './photo/photo.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
     declarations: [
-        PhotoComponent
+        PhotoComponent,
+        PhotoListComponent
     ],
-    exports: [
-        PhotoComponent
-    ],
+    exports: [],
     imports: [
         HttpClientModule
     ],
