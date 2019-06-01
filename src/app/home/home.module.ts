469323ce383bc4f796ef2e35f0ac2ac4e3e8bc4e
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-06-01 01:33:36 -0300

    Delimit the scope of the services

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -9,6 +9,7 @@ import { RouterModule } from '@angular/router';
 import { SignUpComponent } from './signup/signup.component';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
+import { SignUpService } from './signup/signup.service';
 
 @NgModule({
     declarations: [
@@ -24,6 +25,9 @@ import { HomeRoutingModule } from './home.routing.module';
         HttpClientModule,
         RouterModule,
         HomeRoutingModule
+    ],
+    providers: [
+        SignUpService
     ]
 })
 export class HomeModule { }
