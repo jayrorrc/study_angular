6e54b02670c87e9aa291532e5468f0b80a7e8ba1
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-06-01 09:45:09 -0300

    Implement upload photo

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-form',
@@ -11,7 +14,10 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router) { }
 
   ngOnInit() {
     this.photoForm = this.formBuilder.group({
@@ -25,9 +31,9 @@ export class PhotoFormComponent implements OnInit {
     const description = this.photoForm.get('description').value;
     const allowComments = this.photoForm.get('allowComments').value;
 
-    console.log(description);
-    console.log(allowComments);
-    console.log(this.file);
+    this.photoService
+      .upload(description, allowComments, this.file)
+      .subscribe(() => this.router.navigate(['']));
   }
 }
 
