e8f6381369063e210f9cebff6a6862142567a907
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-05-31 22:38:29 -0300

    Crete user service

---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { TokenService } from '../token/token.service';
+import { Subject } from 'rxjs';
+import * as jwt_decode from 'jwt-decode'
+
+import { User } from './user';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+
+    private userSubject = new Subject<User>();
+
+    constructor(private tokenService: TokenService) {
+
+        if (this.tokenService.hasToken()) {
+            this.decodeAndNotify();
+        }
+    }
+
+    private decodeAndNotify() {
+        const token = this.tokenService.getToken();
+        const user = jwt_decode(token) as User;
+        this.userSubject.next(user);
+    }
+
+    setToken(token: string) {
+        this.tokenService.setToken(token);
+        this.decodeAndNotify();
+    }
+
+    getUser() {
+        return this.userSubject.asObservable();
+    }
+}
