690d5945672c282ec25caa33b1ad661323b18c2a
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-06-01 19:59:17 -0300

    Create alert service

---
+++ src/app/shared/components/alert/alert.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Alert, AlertType } from './alert';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AlertService {
+
+    alertSubject: Subject<Alert>;
+
+    private alert(alertType: AlertType, message: string) {
+        this.alertSubject.next(new Alert(alertType, message));
+    }
+
+    success(message: string) {
+        this.alert(AlertType.SUCCESS, message);
+    }
+
+    info(message: string) {
+        this.alert(AlertType.INFO, message);
+    }
+
+    warnig(message: string) {
+        this.alert(AlertType.WARNING, message);
+    }
+
+    danger(message: string) {
+        this.alert(AlertType.DANGER, message);
+    }
+
+    getAlert() {
+        return this.alertSubject.asObservable();
+    }
+}
