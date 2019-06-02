508cbb632788805a71101da7bba3ddb0ff669655
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-06-01 21:07:13 -0300

    Add directive show-if-logged

---
+++ src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, Input, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/user/user.service';
+
+@Directive({
+    selector: '[appShowIfLogged]'
+})
+export class ShowIfLoggedDirective implements OnInit {
+
+
+    constructor(
+        private element: ElementRef<any>,
+        private renderer: Renderer2,
+        private userService: UserService
+    ) {}
+
+    ngOnInit(): void {
+        if (!this.userService.isLogged()) {
+            this.renderer.setStyle(this.element.nativeElement, 'display', 'none');
+        }
+    }
+
+}
