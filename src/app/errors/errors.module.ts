ca5ecb238af68282cbdcc4c4b10e0503a401af98
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-06-04 22:48:19 -0300

    Add page global error

--- src/app/errors/errors.module.ts
+++ src/app/errors/errors.module.ts
@@ -2,13 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
+import { GlobalErrorComponent } from './global-error/global-error.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    NotFoundComponent
+    NotFoundComponent,
+    GlobalErrorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   providers: [
     {
