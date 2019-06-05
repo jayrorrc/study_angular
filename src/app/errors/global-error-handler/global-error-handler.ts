c4aff5133f83071102330bfba25d120276406d87
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-06-04 22:16:52 -0300

    Add more information in error handler

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,9 +1,27 @@
-import { ErrorHandler } from '@angular/core';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import * as StackTrace from 'stacktrace-js';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { UserService } from 'src/app/core/user/user.service';
 
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
+    constructor(
+        private injector: Injector
+    ) { }
+
     handleError(error: any): void {
+
+        const location = this.injector.get(LocationStrategy);
+        const userService = this.injector.get(UserService);
+
+        const url = location instanceof PathLocationStrategy
+            ? location.path()
+            : '';
+
+        const message = error.message
+            ? error.message :
+            error.toString();
 
         StackTrace
             .fromError(error)
@@ -12,7 +30,10 @@ export class GlobalErrorHandler implements ErrorHandler {
                     .map(sf => sf.toString())
                     .join('\n');
 
+                console.log(message);
                 console.log(stackAsString);
+                console.log('o que será enviado para o servidor');
+                console.log({ message, url, userName: userService.getUserName(), stack: stackAsString});
             });
     }
 }
